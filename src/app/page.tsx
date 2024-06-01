acae074738dbb4d1525b5f252e3581b6021062db
elfakhalid <elfatouaki48@gmail.com>
2024-06-01 18:39:51 +0100

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <div>home page</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "2rem",
+      }}
+    >
+      this is an educational project to show how to use Next.js with Server Side
+      Rendering (SSR) and Client Side Rendering (CSR)
+    </div>
+  );
 }
