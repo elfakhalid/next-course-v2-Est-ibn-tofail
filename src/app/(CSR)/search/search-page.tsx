7d1af9ec937fe87d863f7ecd0de1c37b13be504b
elfakhalid <elfatouaki48@gmail.com>
2024-06-01 18:37:43 +0100

    search page

--- src/app/(CSR)/search/search-page.tsx
+++ src/app/(CSR)/search/search-page.tsx
@@ -2,11 +2,11 @@
 
 import { UnsplashSearchResponse } from "@/interfaces/unsplash-search-response";
 import { Button, Form, Input, Row } from "antd";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function SearchPage() {
-  const [searchResults, setSearchResults] =
-    useState<UnsplashSearchResponse | null>(null);
+  const [searchResults, setSearchResults] = useState<any>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const onFinished = async (values: any) => {
@@ -15,7 +15,7 @@ export default function SearchPage() {
     setLoading(false);
     try {
       const results = await fetch(`/api/search?search=${search}`);
-      const images: UnsplashSearchResponse = await results.json();
+      const images: any = await results.json();
       setSearchResults(images);
     } catch (error) {
       console.log(error);
@@ -24,18 +24,33 @@ export default function SearchPage() {
     }
   };
   return (
-    <Row justify="center">
-      <Form onFinish={onFinished}>
-        <Form.Item
-          name="search"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input type="text" placeholder="Search" />
-        </Form.Item>
-        <Button type="primary" htmlType="submit">
-          Search
-        </Button>
-      </Form>
-    </Row>
+    <>
+      <Row justify="center">
+        <Form onFinish={onFinished}>
+          <Form.Item
+            name="search"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input type="text" placeholder="Search" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Search
+          </Button>
+        </Form>
+      </Row>
+      <Row justify="center">
+        {searchResults?.map((result: any) => (
+          <Image
+            key={result.id}
+            src={result.urls.small}
+            alt={result.alt_description}
+            width={200}
+            height={200}
+            objectFit="contain"
+            style={{ margin: "1rem" }}
+          />
+        ))}
+      </Row>
+    </>
   );
 }
